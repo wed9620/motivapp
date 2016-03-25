a74a4ab48e1eaf2e28ac66de25add6d515285499
Shinkarenko Sergei <wed9620@gmail.com>
2016-03-25 12:39:58 +0300

    Initial Commit

---
+++ Motivation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Motivation
+//
+//  Created by Сергей Шинкаренко on 25/03/16.
+//  Copyright © 2016 Sergei Shinkarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
